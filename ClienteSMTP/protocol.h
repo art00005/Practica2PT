0a46d8cc6aeeef7c8472dbfe0c40c8765274ff5d
jbt00002 <jbt00002@red.ujaen.es>
2015-11-16 13:57:45 +0100

    Puertos y maquina de estados
    Modificamos el puerto al TCP_SERVICE_PORT al del SMTP (25). Definimos
    los nuevos estados.

--- ClienteSMTP/protocol.h
+++ ClienteSMTP/protocol.h
@@ -17,15 +17,18 @@
 #define CRLF "\r\n"
 
 //ESTADOS
-#define S_HELO 0
-#define S_USER 1
-#define S_PASS 2
-#define S_DATA 3
-#define S_QUIT 4
-#define S_EXIT 5
+#define S_INI 0
+#define S_HELO 1
+#define S_MAIL 2
+#define S_RCPT 3
+#define S_DATA 4
+#define S_CAB 5
+#define S_SALIR 6
+#define S_QUIT 7
+#define S_EXIT 8
 
 //PUERTO DEL SERVICIO
-#define TCP_SERVICE_PORT	6000
+#define TCP_SERVICE_PORT	25
 
 // NOMBRE Y PASSWORD AUTORIZADOS
 #define USER		"alumno"
